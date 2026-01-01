1ae885071b0f45b3e21bb339f64e14a1ac793b20
11Yazan11 <yazanaljundi05@gmail.com>
2026-01-01 11:53:13 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# sense_hat stuff => https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/1
+
+# Maybe for accelerometer data to compute the orbit since we know the path... Oh that's not dumb lol.
+
+# Gotta try it in the online simulator after doing the code.
+
