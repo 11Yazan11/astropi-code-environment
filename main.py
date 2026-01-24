5c240cbceee5c27b76b5f4518bd412a7e2bd7287
11Yazan11 <yazanaljundi05@gmail.com>
2026-01-24 17:24:07 +0100

    4th commit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# sense_hat stuff => https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/1
-
-# Maybe for accelerometer data to compute the orbit since we know the path
-# I don't think so, the acceleration of the Astro-Pi is none (general relativity physics).
-# The Earth is going up towards the Iss, but the Iss keeps rotating so it never falls, but there is no net acceleration.
-# It just follows some geodesic stuff I guess.
+# https://youtu.be/R7_Fl2kAZD8 (some explanation)
 
 # Gotta try it in the online simulator after doing the code.
 
